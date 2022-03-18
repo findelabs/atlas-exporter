102a3aea51db695c7ffa2fcdb56d67a2e82e556e
Daniel Findell <Daniel.Findell@chrobinson.com>
2022-03-18 10:21:48 -0500

    updated

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .short("p")
                 .long("port")
                 .help("Set port to listen on")
-                .env("RUST_API_LISTEN_PORT")
+                .env("ATLAS_EXPORTER_LISTEN_PORT")
                 .default_value("8080")
                 .takes_value(true),
         )
@@ -45,7 +45,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .long("timeout")
                 .help("Set default global timeout")
                 .default_value("60")
-                .env("RUST_API_TIMEOUT")
+                .env("ATLAS_EXPORTER_TIMEOUT")
                 .takes_value(true),
         )
         .arg(
